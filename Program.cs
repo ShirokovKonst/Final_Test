a5dd103fbaf5599b54880b16bf5dd3a053ec963c
Kostya <ashirokovkonstantin@gmail.com>
2023-09-11 14:19:57 +0500

    Create resultArray

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace C__Final_Test
             int szRez = 0;
             int count = Prompt("Введите количество элементов массива: ");
             string[] arr = FillArray(count);
+            string[] resArr = ResultArray(szRez, arr);
         
 
             int Prompt(string msg)
@@ -30,6 +31,25 @@ namespace C__Final_Test
                 {
                     result[i] = PromptStr("Введите " + i + " элемент массива: ");
                     if (result[i].Length > 3) szRez++; 
+                }
+                return result;
+            }
+
+            string[] ResultArray(int c, string[] arr)
+            {
+                if (c == 0) return null;
+                string[] result = new string[c];
+                int j = 0;
+                while (j < c)
+                {
+                    for (int i = 0; i<arr.Length; i++)
+                    {
+                        if(arr[i].Length < 3)
+                        {   
+                            result[j] = arr[i];
+                            j++;
+                        }
+                    }
                 }
                 return result;
             }
